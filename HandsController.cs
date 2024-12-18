f15d624c51cb8fb053da5e15cc13b4f61cd529ee
bmpq <73074311+bmpq@users.noreply.github.com>
2024-12-18 17:29:44 +0300

    recoil

--- HandsController.cs
+++ HandsController.cs
@@ -10,6 +10,14 @@ namespace ultramove
 
         float coinCooldown;
 
+        Transform palm;
+        Transform recoilPivot;
+        Vector3 recoilHighPos;
+        Quaternion recoilHighRot;
+        Quaternion recoilPivotOriginalLocalQuaternion;
+        Vector3 recoilPivotOriginalLocalPosition;
+        float recoilTime;
+
         public void SetWeapon(GameObject weapon)
         {
             Destroy(weapon.GetComponentInChildren<Animator>());
@@ -37,6 +45,15 @@ namespace ultramove
         void Start()
         {
             animator = GetComponentInChildren<Animator>();
+
+            recoilPivot = transform.FindInChildrenExact("Base HumanRCollarbone");
+            recoilPivotOriginalLocalQuaternion = recoilPivot.localRotation;
+            recoilPivotOriginalLocalPosition = recoilPivot.localPosition;
+
+            recoilHighRot = recoilPivotOriginalLocalQuaternion * Quaternion.Euler(40f, 0, 10f);
+            recoilHighPos = recoilPivotOriginalLocalPosition - new Vector3(0.1f, 0, 0.5f);
+
+            palm = transform.FindInChildrenExact("Base HumanRPalm");
         }
 
         private void Update()
@@ -47,6 +64,30 @@ namespace ultramove
                 if (coinCooldown <= 0f)
                     Coin();
             }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                recoilTime = 0f;
+            }
+
+            recoilTime += Time.deltaTime;
+
+            float recoil = CalculateRecoil(recoilTime, 1f, 50f, 5f);
+            recoilPivot.localRotation = Quaternion.Lerp(recoilPivotOriginalLocalQuaternion, recoilHighRot, recoil);
+            recoilPivot.localPosition = Vector3.Lerp(recoilPivotOriginalLocalPosition, recoilHighPos, recoil);
+        }
+
+        float CalculateRecoil(float time, float intensity, float riseSpeed, float recoveryRate)
+        {
+            riseSpeed = Mathf.Max(0.01f, riseSpeed); // Prevent division issues.
+            recoveryRate = Mathf.Max(0.01f, recoveryRate);
+
+            float rise = 1f - Mathf.Exp(-riseSpeed * time);
+            float fall = Mathf.Exp(-recoveryRate * time);
+
+            float recoil = intensity * rise * fall;
+
+            return Mathf.Max(0f, recoil);
         }
 
         void Coin()
