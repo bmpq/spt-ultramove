755ba5707dd94e08ad03216bbe8fb7e736bba526
bmpq <73074311+bmpq@users.noreply.github.com>
2025-01-07 12:42:00 +0300

    camera shaker delayed shake

--- CameraShaker.cs
+++ CameraShaker.cs
@@ -6,15 +6,29 @@ namespace ultramove
 {
     public class CameraShaker : MonoBehaviour
     {
+        private static CameraShaker instance;
+
         private static float shakeIntensity;
 
         PrismEffects prism;
 
         void Start()
         {
+            instance = this;
             prism = GetComponent<PrismEffects>();
             prism.bloomThreshold = 0f;
             prism.bloomIntensity = 0.5f;
+        }
+
+        private static IEnumerator DelayedShakeCoroutine(float intensity, float delay)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+            Shake(intensity);
+        }
+
+        public static void ShakeAfterDelay(float intensity, float delay)
+        {
+            instance.StartCoroutine(DelayedShakeCoroutine(intensity, delay));
         }
 
         public static void Shake(float intensity)
