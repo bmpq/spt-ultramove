25a8702f348fa01ba5ab720f10354c97a13028a1
bmpq <73074311+bmpq@users.noreply.github.com>
2025-01-10 14:10:28 +0300

    skylight breaking scene

--- V2.cs
+++ V2.cs
@@ -1,4 +1,5 @@
-﻿using Comfort.Common;
+﻿using AssetBundleLoader;
+using Comfort.Common;
 using EFT;
 using System;
 using System.Collections.Generic;
@@ -18,12 +19,23 @@ namespace ultramove
         bool landed;
 
         Animator animator;
+        Animator paneAnimator;
 
         void Awake()
         {
             animator = GetComponent<Animator>();
 
             Singleton<GameWorld>.Instance.MainPlayer.Position = new Vector3(-91.9198f, 27.0865f, 156.2152f);
+
+            paneAnimator = Instantiate(BundleLoader.LoadAssetBundle(BundleLoader.GetDefaultModAssetBundlePath("ultrakill")).LoadAsset<GameObject>("skylight_glass_baked_prefab")).GetComponent<Animator>();
+            Material matGlass = new Material(Shader.Find("Global Fog/Transparent Reflective Specular"));
+            matGlass.mainTexture = BundleLoader.LoadAssetBundle(BundleLoader.GetDefaultModAssetBundlePath("ultrakill")).LoadAsset<Texture>("alpha32");
+            matGlass.SetFloat("_AlphaOffset", 0.45f);
+            matGlass.SetFloat("_Smoothness", 0.8f);
+            foreach (var meshRend in paneAnimator.GetComponentsInChildren<MeshRenderer>())
+            {
+                meshRend.sharedMaterial = matGlass;
+            }
         }
 
         protected override void Revive()
@@ -31,11 +43,11 @@ namespace ultramove
             base.Revive();
 
             TODControl.SetTime(2, 0);
-            TOD_Sky.Instance.Components.Sky.transform.rotation = Quaternion.Euler(54.7627f, 31.5436f, 45.8763f);
+            TOD_Sky.Instance.Components.Sky.transform.rotation = Quaternion.Euler(55.0908f, 32.3366f, 45.8764f);
 
             crashedThroughRoof = false;
             landed = false;
-            transform.position = new Vector3(-91.9198f, 100f, 156.2152f);
+            transform.position = new Vector3(-92.5f, 100f, 156.4f);
             transform.localScale = Vector3.one * 0.5f;
             transform.rotation = Quaternion.Euler(0, 90f, 0);
 
@@ -43,6 +55,12 @@ namespace ultramove
             animator.speed = 0f;
             animator.Rebind();
             animator.Update(0f);
+
+            paneAnimator.gameObject.SetActive(false);
+            paneAnimator.Play("Action", -1);
+            paneAnimator.speed = 0f;
+            paneAnimator.Rebind();
+            paneAnimator.Update(0f);
         }
 
         void Update()
@@ -55,6 +73,10 @@ namespace ultramove
                 crashedThroughRoof = true;
                 PlayGlassCrashEffect();
                 Singleton<Effects>.Instance.Emit("Glass", transform.position, Vector3.down);
+                Singleton<Effects>.Instance.Emit("Glass", transform.position + new Vector3(-0.2f,0,0), Vector3.down * 2f);
+                Singleton<Effects>.Instance.Emit("Glass", transform.position, Vector3.down * 4f);
+                paneAnimator.gameObject.SetActive(true);
+                paneAnimator.speed = 1f;
 
                 Singleton<UltraTime>.Instance.SlowMo(1f, 0.4f);
             }
